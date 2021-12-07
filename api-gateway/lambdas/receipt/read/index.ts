791c2afc407d10edab58a38af141636a9a71b0ec
Balou9 <emmanuel.owosekun@gmail.com>
2021-12-07 02:02:54 +0100

    chore: returns promise

--- api-gateway/lambdas/receipt/read/index.ts
+++ api-gateway/lambdas/receipt/read/index.ts
@@ -1,7 +1,18 @@
-export const handler = async (event: any = {}): Promise<any> => {
-  try {
-    return { statusCode: 200 }
-  } catch (err) {
-    return { err }
-  }
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+} from "aws-lambda";
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+
+  const e : APIGatewayProxyEvent = event
+  return new Promise<APIGatewayProxyResult>((resolve: (arg0: { event: APIGatewayProxyEvent; statusCode: number; }) => void, reject: (arg0: any) => void) => {
+    if (!e) {
+      reject("Error!")
+    } else {
+      resolve({ event: e, statusCode: 200 })
+    }
+  })
 }
