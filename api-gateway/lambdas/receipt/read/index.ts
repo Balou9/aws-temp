afb240cca2cbca3037c296bcf97a98c756cefbef
Balou9 <emmanuel.owosekun@gmail.com>
2021-12-08 22:52:58 +0100

    chore: return 200 status + event

--- api-gateway/lambdas/receipt/read/index.ts
+++ api-gateway/lambdas/receipt/read/index.ts
@@ -6,13 +6,8 @@ import {
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-
-  const e : APIGatewayProxyEvent = event
-  return new Promise<APIGatewayProxyResult>((resolve: (arg0: { event: APIGatewayProxyEvent; statusCode: number; }) => void, reject: (arg0: any) => void) => {
-    if (!e) {
-      reject("Error!")
-    } else {
-      resolve({ event: e, statusCode: 200 })
-    }
-  })
+  const ev = JSON.stringify(event);
+  return {
+    statusCode: 200,
+    body: `Ev: ${ev}`
 }
