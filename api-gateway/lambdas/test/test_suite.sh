15f1ecde55bcf8a1b758b84cc68809f18f5ed3c9
Balou9 <emmanuel.owosekun@gmail.com>
2021-12-15 18:05:38 +0100

    test: tweaks put request

--- api-gateway/lambdas/test/test_suite.sh
+++ api-gateway/lambdas/test/test_suite.sh
@@ -3,12 +3,14 @@ test_receipt_upsert_204() {
   resp_head="$(mktemp)"
   resp_body="$(mktemp)"
 
+  shopping_basket="sb412429"
+
   lurc \
   -X "PUT" \
   -H "content-type: application/json" \
   --data @./api-gateway/lambdas/test/fixtures/shopping_basket.json \
   -D "$resp_head" \
-  "$_BASE_URL/receipt/sb412429"
+  "$_BASE_URL/receipt/$shopping_basket"
   > "$resp_body"
 
   cat "$resp_body"
